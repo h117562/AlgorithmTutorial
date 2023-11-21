4a3b3e7c53cc70d64600ee4df95b11a2d0781402
WooJin <106531904+h117562@users.noreply.github.com>
2023-11-21 09:26:42 +0900

    나이구하기
    나이 age가 주어질 때, 2022년을 기준 출생 연도를 return 하는 solution 함수를 완성하시오
    
    제한사항
    0 < age ≤ 120
    나이는 태어난 연도에 1살이며 1년마다 1씩 증가합니다.

--- AlgorithmTutorial/main.cpp
+++ AlgorithmTutorial/main.cpp
@@ -2,12 +2,19 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int solution(int num1, int num2) {
+int solution(int age) {
     int answer = 0;
+    int year = 2022;//현재 년도
+
+    if (0 < age <= 120)
+    {
+        answer = year - age + 1;
+    }
+
     return answer;
 }
 
 void main()
 {
-    printf("%d\n");
+    printf("%d\n", solution(40));
 }
