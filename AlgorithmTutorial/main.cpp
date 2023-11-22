f4be420fd9e67e2bc74a5d2f6208192ccbfb9a10
WooJin <106531904+h117562@users.noreply.github.com>
2023-11-22 09:32:20 +0900

    숫자 비교하기
    정수 num1과 num2가 매개변수로 주어집니다. 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.

--- AlgorithmTutorial/main.cpp
+++ AlgorithmTutorial/main.cpp
@@ -3,11 +3,14 @@
 #include <stdlib.h>
 
 int solution(int num1, int num2) {
-    int answer = 0;
-    return answer;
+ 
+    if (num1 == num2)
+        return 1;
+
+    return -1;
 }
 
 void main()
 {
-    printf("%d\n");
+    printf("%d\n", solution(11, 11));
 }
