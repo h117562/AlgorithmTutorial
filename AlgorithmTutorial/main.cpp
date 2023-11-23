375061c6be587e5e137f9aef71a21ad67bd861ce
WooJin <106531904+h117562@users.noreply.github.com>
2023-11-23 10:10:35 +0900

    자릿수 더하기
    자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
    예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
    
    제한사항
    N의 범위 : 100,000,000 이하의 자연수

--- AlgorithmTutorial/main.cpp
+++ AlgorithmTutorial/main.cpp
@@ -2,12 +2,22 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int solution(int num1, int num2) {
+int solution(int num1)
+{
     int answer = 0;
+    int tmp = num1;
+
+    for (int i = 100000000; i > 0; i /= 10)
+    {
+        answer += tmp / i;
+        tmp = num1 % i;
+    }
+        
+
     return answer;
 }
 
 void main()
 {
-    printf("%d\n");
+    printf("%d\n", solution(987));
 }
