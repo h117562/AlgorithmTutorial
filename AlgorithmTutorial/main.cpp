39606d7a8a766e78d8edc3a0316a8e182cd33523
WooJin <106531904+h117562@users.noreply.github.com>
2023-11-27 11:06:54 +0900

    3진법 뒤집기
    문제 설명
    자연수 n이 매개변수로 주어집니다. n을 3진법 상에서 앞뒤로 뒤집은 후, 이를 다시 10진법으로 표현한 수를 return 하도록 solution 함수를 완성해주세요.
    
    제한사항
    n은 1 이상 100,000,000 이하인 자연수입니다.

--- AlgorithmTutorial/main.cpp
+++ AlgorithmTutorial/main.cpp
@@ -1,13 +1,36 @@
 ﻿#include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <math.h>
 
-int solution(int num1, int num2) {
-    int answer = 0;
+int solution(int num1) {
+    int answer = num1;
+    int ary[20];
+
+    int i = 0;
+    while (answer > 1)
+    {  
+        ary[i] =answer % 3;
+        answer /= 3;
+
+        i++;
+    }
+
+    ary[i] = answer;
+    answer = 0;
+ 
+
+    for (int j = 0; j <= i; j++)
+    {
+        answer += ary[j] * pow(3, i - j);
+    }
+
+    
     return answer;
 }
 
 void main()
 {
-    printf("%d\n");
+    int num = 100000000;
+    printf("%d, %d\n", num, solution(num));
 }
