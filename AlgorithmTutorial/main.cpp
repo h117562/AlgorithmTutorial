6c4be8acbdf6da26cdc086256009c188f23b15dc
WooJin <106531904+h117562@users.noreply.github.com>
2023-11-28 10:55:14 +0900

    이상한 문자 만들기
    문제 설명
    문자열 s는 한 개 이상의 단어로 구성되어 있습니다. 각 단어는 하나 이상의 공백문자로 구분되어 있습니다. 각 단어의 짝수번째 알파벳은 대문자로, 홀수번째 알파벳은 소문자로 바꾼 문자열을 리턴하는 함수, solution을 완성하세요.
    
    제한 사항
    문자열 전체의 짝/홀수 인덱스가 아니라, 단어(공백을 기준)별로 짝/홀수 인덱스를 판단해야합니다.
    첫 번째 글자는 0번째 인덱스로 보아 짝수번째 알파벳으로 처리해야 합니다.

--- AlgorithmTutorial/main.cpp
+++ AlgorithmTutorial/main.cpp
@@ -1,13 +1,57 @@
 ﻿#include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-int solution(int num1, int num2) {
-    int answer = 0;
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* s) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    char* answer = (char*)malloc(strlen(s) + 1);
+    
+    if (answer != NULL)//exception
+    {
+        strcpy_s(answer, strlen(s) + 1, s);
+    }
+    else
+    {
+        return 0;
+    }
+    
+
+    
+   
+    for (int x = 0; x < strlen(answer); x++)
+    {
+        if (x != 0)
+        {
+            if (*(answer + x) != ' ')
+                continue;
+            x++;
+        }
+
+        for (int y = 0; y < strlen(answer) - x; y++)
+        {
+            
+            if (*(answer +x + y) == ' ')break;
+
+            if (y % 2 == 0)
+            {
+                *(answer + x + y) = toupper(*(answer + x + y));
+            }
+            else
+            {
+                *(answer + x + y) = tolower(*(answer + x + y));
+            }
+        }
+    }
+
     return answer;
 }
 
 void main()
 {
-    printf("%d\n");
+    char* rtn = solution("try hello world");
+    printf("%s", rtn);
+    free(rtn);
 }
