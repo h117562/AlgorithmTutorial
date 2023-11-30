7879974ca694a2d98c390a4503f9e30ea1f3467e
WooJin <106531904+h117562@users.noreply.github.com>
2023-11-30 10:40:22 +0900

    요일 구하기
    문제 설명
    2016년 1월 1일은 금요일입니다. 2016년 a월 b일은 무슨 요일일까요? 두 수 a ,b를 입력받아 2016년 a월 b일이 무슨 요일인지 리턴하는 함수, solution을 완성하세요. 요일의 이름은 일요일부터 토요일까지 각각 SUN,MON,TUE,WED,THU,FRI,SAT
    
    입니다. 예를 들어 a=5, b=24라면 5월 24일은 화요일이므로 문자열 "TUE"를 반환하세요.
    
    제한 조건
    2016년은 윤년입니다.
    2016년 a월 b일은 실제로 있는 날입니다. (13월 26일이나 2월 45일같은 날짜는 주어지지 않습니다)

--- AlgorithmTutorial/main.cpp
+++ AlgorithmTutorial/main.cpp
@@ -1,13 +1,51 @@
 ﻿#include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
+char* solution(int month, int day) {
+    // 리턴할 값은 메모리를 동적 할당해주세요.
+    char* answer = (char*)malloc(4);
+    int frontYear = 20;
+    int lastYear = 16;//16년도 고정
+    int num = 0;
 
-int solution(int num1, int num2) {
-    int answer = 0;
+    //첼러 공식
+    num = day + ((month + 1) * 26 / 10) + lastYear + (lastYear + frontYear) / 4 - (2 * frontYear);
+
+    switch (num % 7)
+    {
+    case 0:
+        strcpy_s(answer, 4, "SAT");
+        break;
+    case 1:
+        strcpy_s(answer, 4, "SUN");
+        break;
+    case 2:
+        strcpy_s(answer, 4, "MON");
+        break;
+    case 3:
+        strcpy_s(answer, 4, "TUE");
+        break;
+    case 4:
+        strcpy_s(answer, 4, "WED");
+        break;
+    case 5:
+        strcpy_s(answer, 4, "THU");
+        break;
+    case 6:
+        strcpy_s(answer, 4, "FRI");
+        break;
+    }
+
     return answer;
 }
 
 void main()
 {
-    printf("%d\n");
+    char* result = solution(12, 5);
+    printf("%s\n", result);
+    free(result);
+    //12월 5일은 월요일
+    //5월 24일은 화요일
+    return;
 }
