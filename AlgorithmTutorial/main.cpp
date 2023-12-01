bc66d6845d4317118a35bf0c19fcb3eb83bcf7e9
WooJin <106531904+h117562@users.noreply.github.com>
2023-12-01 09:40:51 +0900

    서울에서 김서방 찾기
    문제 설명
    String형 배열 seoul의 element중 "Kim"의 위치 x를 찾아, "김서방은 x에 있다"는 String을 반환하는 함수, solution을 완성하세요. seoul에 "Kim"은 오직 한 번만 나타나며 잘못된 값이 입력되는 경우는 없습니다.
    
    제한 사항
    seoul은 길이 1 이상, 1000 이하인 배열입니다.
    seoul의 원소는 길이 1 이상, 20 이하인 문자열입니다.
    "Kim"은 반드시 seoul 안에 포함되어 있습니다.

--- AlgorithmTutorial/main.cpp
+++ AlgorithmTutorial/main.cpp
@@ -1,13 +1,40 @@
-﻿#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
+﻿#include <string>
+#include <vector>
+#include <iostream>
 
-int solution(int num1, int num2) {
-    int answer = 0;
+using namespace std;
+
+string solution(vector<string> seoul) {
+    string answer = " ";
+    string cmp = "Kim";
+
+    if (seoul.empty())
+    {
+        return answer;
+    }
+
+    for (int i = 0; i < seoul.size(); i++)
+    {
+        if (seoul[i].compare(cmp) == 0)
+            answer = "김서방은 " + to_string(i) + "에 있다.";
+    }
+   
+
     return answer;
 }
 
 void main()
 {
-    printf("%d\n");
+    vector<string> seoul;
+  
+    seoul.push_back("Jane");
+    seoul.push_back("Kim");
+    seoul.push_back("asdf");
+    seoul.push_back("nlkfse");
+    seoul.push_back("oiawnfe");
+
+    cout << solution(seoul) << endl;
+
+    seoul.clear();
+    vector<string>().swap(seoul);
 }
