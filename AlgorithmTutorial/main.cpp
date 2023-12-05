cc400b1d5f962737496ad23267722992bba6ecc4
WooJin <106531904+h117562@users.noreply.github.com>
2023-12-06 00:11:19 +0900

    에어컨
    문제 설명
    현대모비스에서 개발한 실내공조 제어 시스템은 차내에 승객이 탑승 중일 때 항상 쾌적한 실내온도(t1 ~ t21)를 유지할 수 있도록 합니다. 현재(0분) 실내온도는 실외온도와 같습니다.
    
    실내공조 제어 시스템은 실내온도를 조절하기 위해 에어컨의 전원을 켜 희망온도를 설정합니다. 희망온도는 에어컨의 전원이 켜져 있는 동안 원하는 값으로 변경할 수 있습니다. 실내온도와 희망온도가 다르다면 1분 뒤 실내온도가 희망온도와 같아지는 방향으로 1도 상승 또는 하강합니다. 실내온도가 희망온도와 같다면 에어컨이 켜져 있는 동안은 실내온도가 변하지 않습니다.
    
    에어컨의 전원을 끄면 실내온도가 실외온도와 같아지는 방향으로 매 분 1도 상승 또는 하강합니다. 실내온도와 실외온도가 같다면 실내온도는 변하지 않습니다.
    
    에어컨의 소비전력은 현재 실내온도에 따라 달라집니다. 에어컨의 희망온도와 실내온도가 다르다면 매 분 전력을 a만큼 소비하고, 희망온도와 실내온도가 같다면 매 분 전력을 b만큼 소비합니다. 에어컨이 꺼져 있다면 전력을 소비하지 않으며, 에어컨을 켜고 끄는데 필요한 시간과 전력은 0이라고 가정합니다.
    
    실내공조 제어 시스템은 차내에 승객이 탑승 중일 때 실내온도를 t1 ~ t2도 사이로 유지하면서, 에어컨의 소비전력을 최소화합니다.

--- AlgorithmTutorial/main.cpp
+++ AlgorithmTutorial/main.cpp
@@ -1,13 +1,93 @@
-﻿#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
+﻿#include <string>
+#include <vector>
+#include <iostream>
 
-int solution(int num1, int num2) {
+using namespace std;
+
+int solution(int temperature, int t1, int t2, int a, int b, vector<int> onboard) {
     int answer = 0;
+    int near = 0;
+    int temp = temperature;
+  
+    near = abs(temp - t1) < abs(temp - t2) ? t1 : t2;//t1, t2 중 근사값 찾기
+
+    for (int i = 0; i < onboard.size(); i++)
+    {
+        if (onboard[i] == 1)//에어컨이 켜있을 때,
+        {
+
+            if (temp == near)
+            {
+                answer += b;//실내, 희망 온도가 같을 경우 b전력을 소비하여 유지
+
+                continue;//이후 연산 무시하고 건너뛰기
+            }
+           
+            temp < near ? temp++ : temp--;//실내 온도가 희망 온도와 다를 경우 a전력 소비하여 희망 온도와 가까워짐
+            answer += a;
+        }
+        else//에어컨 껏을 때,
+        {
+           
+            if (temp == temperature)//실내, 실외 온도가 다를 경우 1씩 실외 온도와 가까워짐
+            {
+                temp < temperature ? temp++ : temp--;
+            }
+        }
+        
+        
+    }
+
     return answer;
+}
+
+void print(int temperature, int t1, int t2, int a, int b, vector<int> onboard, int result)
+{
+    cout.width(11);
+    cout << temperature << " ";
+
+    cout.width(3);
+    cout << t1 << " ";
+
+    cout.width(3);
+    cout << t2 << " ";
+
+    cout.width(3);
+    cout << a << " ";
+
+    cout.width(3);
+    cout << b << " ";
+
+    cout.width(6);
+    cout << result << "     ";
+    
+    cout << "[";
+    for (int i = 0; i < onboard.size(); i++)
+    {
+        cout << onboard[i] << ", ";
+    }
+    cout << "]" << endl;
+
+
 }
 
 void main()
 {
-    printf("%d\n");
+    vector<int> list;
+    int temp, t1, t2, a, b, answer;
+    
+   
+    cout << "temperature  t1  t2   a   b result  onboard" << endl;
+
+    list = { 0 ,0 ,1 ,1 ,1 ,1 ,1 };
+    temp = 28; t1 = 18; t2 = 26; a = 10; b = 8; 
+    answer = solution(temp, t1, t2, a, b, list);
+    print(temp, t1, t2, a, b, list, answer);
+
+    list = { 0 ,0 ,0 ,0 ,0 ,1 ,0 };
+    temp = -10; t1 = -5; t2 = 5; a = 5; b = 1;
+    answer = solution(temp, t1, t2, a, b, list);
+    print(temp, t1, t2, a, b, list, answer);
+
+
 }
