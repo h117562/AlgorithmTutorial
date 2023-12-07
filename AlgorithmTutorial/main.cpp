493950de053770db3473655176cb553cbdc3ddc2
WooJin <106531904+h117562@users.noreply.github.com>
2023-12-07 14:10:03 +0900

    정수 내림차순으로 배치하기
    문제 설명
    함수 solution은 정수 n을 매개변수로 입력받습니다. n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요. 예를들어 n이 118372면 873211을 리턴하면 됩니다.
    
    제한 조건
    n은 1이상 8000000000 이하인 자연수입니다.

--- AlgorithmTutorial/main.cpp
+++ AlgorithmTutorial/main.cpp
@@ -1,13 +1,53 @@
-﻿#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
+﻿#include <string>
+#include <vector>
+#include <iostream>
 
-int solution(int num1, int num2) {
-    int answer = 0;
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+    long long temp = n;
+    vector<int> ary;
+
+    for (int i = 1000000000; i >= 1; i /= 10) //자리수 마다 구별하여 배열에 넣는다
+    {
+        if (temp % i != n)
+        {
+            ary.push_back(temp / i);
+            temp %= i;
+        }
+    }
+
+
+    for (int i = 0; i < ary.size() - 1; i++)//버블 정렬
+    {
+        for (int j = 1; j < ary.size() - i; j++)
+        {
+            if (ary[j - 1] < ary[j])//내림차 순
+            {
+                swap(ary[j - 1], ary[j]);
+            }
+        }
+    }
+
+
+    for (int i = 0; i < ary.size(); i++)
+    {
+        answer *= 10;
+        answer += ary[i];
+    }
+    
+    ary.clear();
+    vector<int>().swap(ary);
+
     return answer;
 }
 
+
+
 void main()
 {
-    printf("%d\n");
+    long long result = 0;
+    result = solution(123515);
+    cout << result;
 }
