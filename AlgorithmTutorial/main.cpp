6f16145ab6406196a794ce52831b9a3ca1b9d57d
WooJin <106531904+h117562@users.noreply.github.com>
2023-12-11 11:41:46 +0900

    사라지는 발판
    문제 설명
    문제 설명
    플레이어 A와 플레이어 B가 서로 게임을 합니다. 당신은 이 게임이 끝날 때까지 양 플레이어가 캐릭터를 몇 번 움직이게 될지 예측하려고 합니다.
    
    각 플레이어는 자신의 캐릭터 하나를 보드 위에 올려놓고 게임을 시작합니다. 게임 보드는 1x1 크기 정사각 격자로 이루어져 있으며, 보드 안에는 발판이 있는 부분과 없는 부분이 있습니다. 발판이 있는 곳에만 캐릭터가 서있을 수 있으며, 처음 캐릭터를 올려놓는 곳은 항상 발판이 있는 곳입니다. 캐릭터는 발판이 있는 곳으로만 이동할 수 있으며, 보드 밖으로 이동할 수 없습니다. 밟고 있던 발판은 그 위에 있던 캐릭터가 다른 곳으로 이동하여 다른 발판을 밞음과 동시에 사라집니다. 양 플레이어는 번갈아가며 자기 차례에 자신의 캐릭터를 상하좌우로 인접한 4개의 칸 중에서 발판이 있는 칸으로 옮겨야 합니다.
    
    다음과 같은 2가지 상황에서 패자와 승자가 정해지며, 게임이 종료됩니다.
    
    움직일 차례인데 캐릭터의 상하좌우 주변 4칸이 모두 발판이 없거나 보드 밖이라서 이동할 수 없는 경우, 해당 차례 플레이어는 패배합니다.
    두 캐릭터가 같은 발판 위에 있을 때, 상대 플레이어의 캐릭터가 다른 발판으로 이동하여 자신의 캐릭터가 서있던 발판이 사라지게 되면 패배합니다.
    게임은 항상 플레이어 A가 먼저 시작합니다. 양 플레이어는 최적의 플레이를 합니다. 즉, 이길 수 있는 플레이어는 최대한 빨리 승리하도록 플레이하고, 질 수밖에 없는 플레이어는 최대한 오래 버티도록 플레이합니다. '이길 수 있는 플레이어'는 실수만 하지 않는다면 항상 이기는 플레이어를 의미하며, '질 수밖에 없는 플레이어'는 최선을 다해도 상대가 실수하지 않으면 항상 질 수밖에 없는 플레이어를 의미합니다. 최대한 오래 버틴다는 것은 양 플레이어가 캐릭터를 움직이는 횟수를 최대화한다는 것을 의미합니다.

--- AlgorithmTutorial/main.cpp
+++ AlgorithmTutorial/main.cpp
@@ -1,13 +1,322 @@
-﻿#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
 
-int solution(int num1, int num2) {
-    int answer = 0;
+using namespace std;
+
+struct Point
+{
+    int x;
+    int y;
+
+    Point(int x, int y)
+    {
+        this->x = x;
+        this->y = y;
+    }
+
+};
+
+enum Direction//m_compare 알아보기 쉽게
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+};
+
+
+//가장 턴수가 적게 드는 경우 탐색
+int solution(vector<vector<int>>& m_beforeboard, Point& m_first, Point m_last, int m_level) 
+{
+    int answer = m_level;//진행된 턴 수
+    vector<vector<int>> m_newboard;
+    Point m_nextposition = m_first;
+    int m_compare[4] = {0,0,0,0};
+    m_newboard = m_beforeboard;//보드판 복사
+    m_newboard[m_first.y][m_first.x] = 0;//원래 있던 자리는 0으로 만들고 이동하고 다음 패턴 예측
+    answer++;
+    
+    
+    if (m_first.x == m_last.x && m_first.y == m_last.y)//패배 확정일 경우 쓸데없는 연산 무시하고 0리턴
+    {
+        return 0;
+    }
+   
+        if (0 <= m_first.y - 1)//위쪽으로 이동했을 경우 턴 예측
+        {
+            if (m_newboard[m_first.y - 1][m_first.x] != 0)
+            {
+                m_nextposition = Point(m_first.x, m_first.y - 1);
+                m_compare[UP] = solution(m_newboard, m_last, m_nextposition, answer);
+            }
+            
+        }
+
+        if (m_newboard.size() > m_first.y + 1)//아래쪽으로 이동했을 경우 턴 예측
+        {
+            if (m_newboard[m_first.y + 1][m_first.x] != 0)
+            {
+                m_nextposition = Point(m_first.x, m_first.y + 1);
+                m_compare[DOWN] = solution(m_newboard, m_last, m_nextposition, answer);
+            }
+              
+        }
+    
+        if (0 <= m_first.x - 1)//왼쪽으로 이동했을 경우 턴 예측
+        {
+            if (m_newboard[m_first.y][m_first.x - 1] != 0)
+            {
+                m_nextposition = Point(m_first.x - 1, m_first.y);
+                m_compare[LEFT] = solution(m_newboard, m_last, m_nextposition, answer);
+            }
+        }
+ 
+        if (m_newboard[0].size() > m_first.x + 1)//오른쪽으로 이동했을 경우 턴 예측
+        {
+            if (m_newboard[m_first.y][m_first.x + 1] != 0)
+            {
+                m_nextposition = Point(m_first.x + 1, m_first.y);
+                m_compare[RIGHT] = solution(m_newboard, m_last, m_nextposition, answer);
+            }
+        }
+
+        int min = INT_MAX;
+
+        for (int obj : m_compare)
+        {
+            if (obj < min && obj != 0)
+                min = obj;
+        }
+
+        if (answer == 1)//최상위 노드일 경우
+        {
+            int m_direction = 5;
+
+            for (int i = 0; i < 4; i++)//어느 방향으로 이동해야 하는지 찾기
+            {
+                if (m_compare[i]==min)
+                {
+                    m_direction = i;
+                }
+            }
+
+            m_beforeboard[m_first.y][m_first.x] = 0;//이전 위치는 0으로 만들고 이동
+
+            switch (m_direction)//이동
+            {
+            case UP:
+                m_nextposition = Point(m_first.x, m_first.y - 1);
+                m_first = m_nextposition;
+                break;
+            case DOWN:
+                 m_nextposition = Point(m_first.x, m_first.y + 1);
+                 m_first = m_nextposition;
+                break;
+            case LEFT:
+                 m_nextposition = Point(m_first.x - 1, m_first.y);
+                 m_first = m_nextposition;
+                break;
+            case RIGHT:
+                 m_nextposition = Point(m_first.x + 1, m_first.y);
+                 m_first = m_nextposition;
+                break;
+            default:
+                cout << "이동할 곳이 없습니다." << endl;
+                return 0; 
+                break;
+            }
+        }
+
+        if (min != INT_MAX)
+            answer = min;//가장 작은 턴수를 리턴
+
+        m_newboard.clear();
+        vector<vector<int>>().swap(m_newboard);
+
     return answer;
 }
 
+int solution2(vector<vector<int>>& m_beforeboard, Point& m_first, Point m_last, int m_level)
+{
+    int answer = m_level;//진행된 턴 수
+    vector<vector<int>> m_newboard;
+    Point m_nextposition = m_first;
+    int m_compare[4] = { 0,0,0,0 };
+    m_newboard = m_beforeboard;//보드판 복사
+    m_newboard[m_first.y][m_first.x] = 0;//원래 있던 자리는 0으로 만들고 이동하고 다음 패턴 예측
+    answer++;
+
+    if (m_first.x == m_last.x && m_first.y == m_last.y)//패배 확정일 경우 쓸데없는 연산 무시하고 0리턴
+    {
+        return 0;
+    }
+
+    if (0 <= m_first.y - 1)//위쪽으로 이동했을 경우 턴 예측
+    {
+        if (m_newboard[m_first.y - 1][m_first.x] != 0)
+        {
+            m_nextposition = Point(m_first.x, m_first.y - 1);
+            m_compare[UP] = solution2(m_newboard, m_last, m_nextposition, answer);
+        }
+
+    }
+
+    if (m_newboard.size() > m_first.y + 1)//아래쪽으로 이동했을 경우 턴 예측
+    {
+        if (m_newboard[m_first.y + 1][m_first.x] != 0)
+        {
+            m_nextposition = Point(m_first.x, m_first.y + 1);
+            m_compare[DOWN] = solution2(m_newboard, m_last, m_nextposition, answer);
+        }
+
+    }
+
+    if (0 <= m_first.x - 1)//왼쪽으로 이동했을 경우 턴 예측
+    {
+        if (m_newboard[m_first.y][m_first.x - 1] != 0)
+        {
+            m_nextposition = Point(m_first.x - 1, m_first.y);
+            m_compare[LEFT] = solution2(m_newboard, m_last, m_nextposition, answer);
+        }
+    }
+
+    if (m_newboard[0].size() > m_first.x + 1)//오른쪽으로 이동했을 경우 턴 예측
+    {
+        if (m_newboard[m_first.y][m_first.x + 1] != 0)
+        {
+            m_nextposition = Point(m_first.x + 1, m_first.y);
+            m_compare[RIGHT] = solution2(m_newboard, m_last, m_nextposition, answer);
+        }
+    }
+
+    int max = 0;
+
+    for (int obj : m_compare)
+    {
+        if (obj > max)
+            max = obj;
+    }
+
+    if (answer == 1)//최상위 노드일 경우
+    {
+        int m_direction;
+
+        for (int i = 0; i < 4; i++)//어느 방향으로 이동해야 하는지 찾기
+        {
+            if (m_compare[i] == max)
+            {
+                m_direction = i;
+            }
+
+            if (max == 0)//이동할 곳이 없으면
+                m_direction = 5; //default로
+        }
+
+        m_beforeboard[m_first.y][m_first.x] = 0;//이전 위치는 0으로 만들고 이동
+
+        switch (m_direction)//이동
+        {
+        case UP:
+            m_nextposition = Point(m_first.x, m_first.y - 1);
+            m_first = m_nextposition;
+            break;
+        case DOWN:
+            m_nextposition = Point(m_first.x, m_first.y + 1);
+            m_first = m_nextposition;
+            break;
+        case LEFT:
+            m_nextposition = Point(m_first.x - 1, m_first.y);
+            m_first = m_nextposition;
+            break;
+        case RIGHT:
+            m_nextposition = Point(m_first.x + 1, m_first.y);
+            m_first = m_nextposition;
+            break;
+        default:
+            cout << "이동할 곳이 없습니다." << endl;
+            break;
+        }
+
+
+    }
+
+    if (max != 0)
+        answer = max;//가장 작은 턴수를 리턴
+
+    m_newboard.clear();
+    vector<vector<int>>().swap(m_newboard);
+
+    return answer;
+}
+
+void ShowBoard(vector<vector<int>> m_board, Point m_player1, Point m_player2)
+{
+    
+    for (int i = 0; i < m_board.size(); i++)
+    {
+        for (int j = 0; j < m_board[0].size(); j++)
+        {
+            cout.width(2);
+            if (m_player1.x == j && m_player1.y == i)
+            {
+                cout << "■";
+            }else if (m_player2.x == j && m_player2.y == i)
+            {
+                cout << "□";
+            }
+            else
+            {
+                cout << m_board[i][j];
+            }
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+
+//대각선 이동 불가능
+//땅따먹기 일종의 게임
 void main()
 {
-    printf("%d\n");
+    vector<vector<int>> m_board;//보드 원본
+    Point m_player1(0, 2), m_player2(4, 2);//두 플레이어의 초기 위치
+    
+    m_board = { {1,1,1,1,1},{1,1,1,1,1},{1,1,1,1,1},{1,1,1,1,1},{1,1,1,1,1} }; //5x5 보드
+
+    solution(m_board, m_player1, m_player2, 0);//가장 적게 걸리는 턴만 계산
+    ShowBoard(m_board, m_player1, m_player2);//보드판 출력
+    solution2(m_board, m_player2, m_player1, 0);//가장 오래 걸리는 턴만 계산
+    ShowBoard(m_board, m_player1, m_player2);//보드판 출력
+    solution(m_board, m_player1, m_player2, 0);//가장 적게 걸리는 턴만 계산
+    ShowBoard(m_board, m_player1, m_player2);//보드판 출력
+    solution2(m_board, m_player2, m_player1, 0);//가장 오래 걸리는 턴만 계산
+    ShowBoard(m_board, m_player1, m_player2);//보드판 출력
+    solution(m_board, m_player1, m_player2, 0);//가장 적게 걸리는 턴만 계산
+    ShowBoard(m_board, m_player1, m_player2);//보드판 출력
+    solution2(m_board, m_player2, m_player1, 0);//가장 오래 걸리는 턴만 계산
+    ShowBoard(m_board, m_player1, m_player2);//보드판 출력
+    solution(m_board, m_player1, m_player2, 0);//가장 적게 걸리는 턴만 계산
+    ShowBoard(m_board, m_player1, m_player2);//보드판 출력
+    solution2(m_board, m_player2, m_player1, 0);//가장 오래 걸리는 턴만 계산
+    ShowBoard(m_board, m_player1, m_player2);//보드판 출력
+    solution(m_board, m_player1, m_player2, 0);//가장 적게 걸리는 턴만 계산
+    ShowBoard(m_board, m_player1, m_player2);//보드판 출력
+    solution2(m_board, m_player2, m_player1, 0);//가장 오래 걸리는 턴만 계산
+    ShowBoard(m_board, m_player1, m_player2);//보드판 출력
+    solution(m_board, m_player1, m_player2, 0);//가장 적게 걸리는 턴만 계산
+    ShowBoard(m_board, m_player1, m_player2);//보드판 출력
+    solution2(m_board, m_player2, m_player1, 0);//가장 오래 걸리는 턴만 계산
+    ShowBoard(m_board, m_player1, m_player2);//보드판 출력
+    solution(m_board, m_player1, m_player2, 0);//가장 적게 걸리는 턴만 계산
+    ShowBoard(m_board, m_player1, m_player2);//보드판 출력
+    solution2(m_board, m_player2, m_player1, 0);//가장 오래 걸리는 턴만 계산
+    ShowBoard(m_board, m_player1, m_player2);//보드판 출력
+    solution(m_board, m_player1, m_player2, 0);//가장 적게 걸리는 턴만 계산
+    ShowBoard(m_board, m_player1, m_player2);//보드판 출력
+    solution2(m_board, m_player2, m_player1, 0);//가장 오래 걸리는 턴만 계산
+    ShowBoard(m_board, m_player1, m_player2);//보드판 출력
 }
+
