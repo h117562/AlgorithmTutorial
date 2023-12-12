f5b8935a7a2ce2a70687dc5ef6059c1498a1aae1
WooJin <106531904+h117562@users.noreply.github.com>
2023-12-12 10:08:15 +0900

    행렬의 덧셈
    문제 설명
    행렬의 덧셈은 행과 열의 크기가 같은 두 행렬의 같은 행, 같은 열의 값을 서로 더한 결과가 됩니다. 2개의 행렬 arr1과 arr2를 입력받아, 행렬 덧셈의 결과를 반환하는 함수, solution을 완성해주세요.
    
    제한 조건
    행렬 arr1, arr2의 행과 열의 길이는 500을 넘지 않습니다.

--- AlgorithmTutorial/main.cpp
+++ AlgorithmTutorial/main.cpp
@@ -1,13 +1,42 @@
-﻿#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
+﻿#include <iostream>
+#include <vector>
 
-int solution(int num1, int num2) {
-    int answer = 0;
-    return answer;
+using namespace std;
+
+void solution(vector<vector<int>>& answer, vector<vector<int>> arr1, vector<vector<int>> arr2) 
+{
+    answer = arr1;
+
+    if (arr1.size() != arr2.size() && arr1[0].size() != arr2[0].size())//두 행렬 사이즈가 다를 때
+        return;
+
+    
+    for (int i = 0; i < arr1.size(); i++)
+    {
+        for (int j = 0; j < arr1[0].size(); j++)
+        {
+            answer[i][j] = answer[i][j] + arr2[i][j];
+        }
+    }
+
+    return;
 }
 
 void main()
 {
-    printf("%d\n");
+    vector<vector<int>> arr1 = { {1,2,3},{1,2,3} };
+    vector<vector<int>> arr2 = { {1,2,3},{1,2,3} };
+    vector<vector<int>> sum;
+
+    solution(sum, arr1, arr2);
+
+
+    for (int i = 0; i < sum.size(); i++)
+    {
+        for (int j = 0; j < sum[0].size(); j++)
+        {
+            cout << sum[i][j] << ", ";
+        }
+        cout << endl;
+    }
 }
