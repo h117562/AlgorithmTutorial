02c251e16dcc4f511efe93b54f56e7888b378fb8
WooJin <106531904+h117562@users.noreply.github.com>
2023-12-15 11:03:16 +0900

    체육복
    문제 설명
    점심시간에 도둑이 들어, 일부 학생이 체육복을 도난당했습니다. 다행히 여벌 체육복이 있는 학생이 이들에게 체육복을 빌려주려 합니다. 학생들의 번호는 체격 순으로 매겨져 있어, 바로 앞번호의 학생이나 바로 뒷번호의 학생에게만 체육복을 빌려줄 수 있습니다. 예를 들어, 4번 학생은 3번 학생이나 5번 학생에게만 체육복을 빌려줄 수 있습니다. 체육복이 없으면 수업을 들을 수 없기 때문에 체육복을 적절히 빌려 최대한 많은 학생이 체육수업을 들어야 합니다.
    
    전체 학생의 수 n, 체육복을 도난당한 학생들의 번호가 담긴 배열 lost, 여벌의 체육복을 가져온 학생들의 번호가 담긴 배열 reserve가 매개변수로 주어질 때, 체육수업을 들을 수 있는 학생의 최댓값을 return 하도록 solution 함수를 작성해주세요.
    
    제한사항
    전체 학생의 수는 2명 이상 30명 이하입니다.
    체육복을 도난당한 학생의 수는 1명 이상 n명 이하이고 중복되는 번호는 없습니다.
    여벌의 체육복을 가져온 학생의 수는 1명 이상 n명 이하이고 중복되는 번호는 없습니다.
    여벌 체육복이 있는 학생만 다른 학생에게 체육복을 빌려줄 수 있습니다.
    여벌 체육복을 가져온 학생이 체육복을 도난당했을 수 있습니다. 이때 이 학생은 체육복을 하나만 도난당했다고 가정하며, 남은 체육복이 하나이기에 다른 학생에게는 체육복을 빌려줄 수 없습니다.

--- AlgorithmTutorial/main.cpp
+++ AlgorithmTutorial/main.cpp
@@ -1,13 +1,47 @@
-﻿#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
+﻿#include <string>
+#include <vector>
+#include <iostream>
 
-int solution(int num1, int num2) {
+using namespace std;
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
     int answer = 0;
+    answer = reserve.size();//체육복을 이미 갖고 있던 학생들 수 
+
+    for (int n_lost : lost)
+    {
+        if (0 <= n_lost - 1)
+        {
+            for (int n_reserve : reserve)
+            {
+                if (n_reserve == n_lost - 1)//학생 앞 번호가 여분이 있을 때
+                {
+                    answer++;//체육복을 갖고 있는 학생 수 증가
+
+                    break;
+                }
+                else if(n_reserve == n_lost + 1)//학생 뒷 번호가 여분이 있을 때
+                {
+                    answer++;//체육복을 갖고 있는 학생 수 증가
+
+                    break;
+                }
+            }
+        }
+
+
+    }
+
     return answer;
 }
 
+//학생 번호는 1부터 시작 30이 최대
 void main()
 {
-    printf("%d\n");
+    vector<int> lost = { 2, 4 };
+    vector<int> reserve = { 1,3,5 };
+
+    int result = 0;
+    result = solution(5, lost, reserve);
+    cout << result;
 }
