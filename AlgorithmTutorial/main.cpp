1d54061a50f56375788f9c5ed374a5b1939230ce
WooJin <106531904+h117562@users.noreply.github.com>
2023-12-19 09:38:50 +0900

    직사각형 별찍기
    문제 설명
    이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다.
    별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인 직사각형 형태를 출력해보세요.
    
    제한 조건
    n과 m은 각각 1000 이하인 자연수입니다.

--- AlgorithmTutorial/main.cpp
+++ AlgorithmTutorial/main.cpp
@@ -1,13 +1,46 @@
-﻿#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
+﻿#include <iostream>
+#include <vector>
 
-int solution(int num1, int num2) {
-    int answer = 0;
-    return answer;
+using namespace std;
+
+void solution(int num1, int num2) {
+	vector<vector<char>> stars;
+
+	for (int y = 0; y < num1; y++)
+	{
+		stars.push_back(vector<char>());//열 추가
+
+		for (int x = 0; x < num2; x++)
+		{
+			stars[y].push_back('*');
+		}
+	}
+
+
+	for (int y = 0; y < stars.size(); y++)
+	{
+		for (int x = 0; x < stars[0].size(); x++)
+		{
+			cout << stars[y][x];
+		}
+
+		cout << endl;
+	}
+
+
+	stars.clear();
+	vector<vector<char>>().swap(stars);
+	return;
 }
 
 void main()
 {
-    printf("%d\n");
+	int a, b = 0;
+
+	cin >> a >> b;
+	cout << a << ", " << b << endl;
+
+	solution(a, b);
+
+	return;
 }
