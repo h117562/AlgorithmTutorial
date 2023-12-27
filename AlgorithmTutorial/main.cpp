e67d51e4218897cdec40d3c6fd37d2243be915b2
WooJin <106531904+h117562@users.noreply.github.com>
2023-12-27 10:37:00 +0900

    시저 암호
    문제 설명
    어떤 문장의 각 알파벳을 일정한 거리만큼 밀어서 다른 알파벳으로 바꾸는 암호화 방식을 시저 암호라고 합니다. 예를 들어 "AB"는 1만큼 밀면 "BC"가 되고, 3만큼 밀면 "DE"가 됩니다. "z"는 1만큼 밀면 "a"가 됩니다. 문자열 s와 거리 n을 입력받아 s를 n만큼 민 암호문을 만드는 함수, solution을 완성해 보세요.
    
    제한 조건
    공백은 아무리 밀어도 공백입니다.
    s는 알파벳 소문자, 대문자, 공백으로만 이루어져 있습니다.
    s의 길이는 8000이하입니다.
    n은 1 이상, 25이하인 자연수입니다.

--- AlgorithmTutorial/main.cpp
+++ AlgorithmTutorial/main.cpp
@@ -1,13 +1,39 @@
-﻿#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
+﻿#include <iostream>
+#include <vector>
 
-int solution(int num1, int num2) {
-    int answer = 0;
+using namespace std;
+
+
+//n만큼 알파벳을 뒤로 밀어서 다른 알파벳으로 바꾸는 함수
+string solution(string str, int n) {
+    string answer = "";
+
+    for (char c : str)
+    {
+        if (c != ' ')//공백 제외
+        {
+            if (c + n > 122)//z를 넘어갔을 땐 다시 A로 돌아가서 시작
+            {
+                answer += 65 + (c + n - 123);
+            }
+            else
+            {
+                answer += c + n;
+            }
+        }
+        else
+        {//공백은 그대로 삽입
+            answer += ' ';
+        }
+    }
+
+    cout << answer;
     return answer;
 }
 
 void main()
 {
-    printf("%d\n");
+
+    solution("aaf y", 2);
+    
 }
