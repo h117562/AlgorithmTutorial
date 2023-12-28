df35ca731825d20cadb18c5a1781298f0aad2323
WooJin <106531904+h117562@users.noreply.github.com>
2023-12-28 11:12:47 +0900

    숫자 변환
    문제 설명
    자연수 x를 y로 변환하려고 합니다. 사용할 수 있는 연산은 다음과 같습니다.
    
    x에 n을 더합니다
    x에 2를 곱합니다.
    x에 3을 곱합니다.
    자연수 x, y, n이 매개변수로 주어질 때, x를 y로 변환하기 위해 필요한 최소 연산 횟수를 return하도록 solution 함수를 완성해주세요. 이때 x를 y로 만들 수 없다면 -1을 return 해주세요.

--- AlgorithmTutorial/main.cpp
+++ AlgorithmTutorial/main.cpp
@@ -1,13 +1,63 @@
-﻿#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
+﻿#include <iostream>
 
-int solution(int num1, int num2) {
-    int answer = 0;
-    return answer;
+using namespace std;
+
+
+int PlusN(int x, int n)
+{
+    return x + n;
 }
 
-void main()
+int Mul2(int x)
 {
-    printf("%d\n");
+    return x * 2;
+}
+
+int Mul3(int x)
+{
+    return x * 3;
+}
+
+//x를 y로 변환하기 위한 최소 계산 루트를 찾는 함수
+int solution(int x, int y, int n, int beforelevel) 
+{
+    int level = beforelevel;
+    int result[3];
+
+    if (x == y)
+    {
+        return level;
+    }
+
+    if (x > y)
+    {
+        return -1;
+    }
+
+    level++;
+    result[0] = solution(PlusN(x, n), y, n, level);//합계산 루트
+    result[1] = solution(Mul2(x), y, n, level);//2곱 루트
+    result[2] = solution(Mul3(x), y, n, level);//3곱 루트
+    
+    int min = INT_MAX;
+
+    for (int a : result)
+    {
+        if (min > a && a > 0)
+        {
+            min = a;
+        }
+    }
+
+    if (min == INT_MAX)//최소값이 -1 제외하고 없으면
+        return -1;
+    
+    return min;
+}
+
+int main()
+{
+    cout << solution(10, 40, 6, 0);
+
+    return 0;
 }
